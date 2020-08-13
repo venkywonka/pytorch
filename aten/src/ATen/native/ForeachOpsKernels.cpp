7735e261baca2186ad10dd933a1e121e27bdd659
Iurii Zdebskyi <iuriiz@devfair004.maas>
2020-08-12 19:52:05 -0700

    Added add_scalar_
    ghstack-source-id: 9a4999cc7a836269c450fd72ad8b24144bf225e5
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/42531

--- aten/src/ATen/native/ForeachOpsKernels.cpp
+++ aten/src/ATen/native/ForeachOpsKernels.cpp
@@ -1,14 +1,25 @@
 #include <ATen/ATen.h>
+#include <ATen/native/ForeachUtils.h>
+
 namespace at { namespace native {
 
-std::vector<Tensor> foreach_add_scalar_kernel_fallback(TensorList tensors, Scalar scalar) {
-  TORCH_CHECK(tensors.size() > 0, "Tensor list must have at least one tensor.");
+std::vector<Tensor> foreach_tensor_add_scalar_kernel_slow(TensorList tensors, Scalar scalar) {
+  verify_list(tensors);
 
   std::vector<Tensor> result;
-  for (int i = 0; i < tensors.size(); i++) {
-    auto temp = tensors[i].add(scalar);
+  for (const auto& t : tensors) {
+    auto temp = t.add(scalar);
     result.emplace_back(temp);
   }
   return result;
 }
+
+void foreach_tensor_add_scalar_kernel_slow_(TensorList tensors, Scalar scalar) {
+  verify_list(tensors);
+
+  for (auto& t : tensors) {
+    t.add_(scalar);
+  }
+}
+
 }} // namespace at::native
---
+++ aten/src/ATen/native/ForeachUtils.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <ATen/ATen.h>
+
+namespace at { 
+namespace native {
+
+void verify_list(TensorList tensors) {
+  TORCH_CHECK(tensors.size() > 0, "Tensor list must have at least one tensor.");
+  auto expected_dtype = tensors[0].dtype();
+  auto expected_device = tensors[0].device();
+  auto expected_sizes = tensors[0].sizes();
+
+  for (auto t : tensors) {
+    TORCH_CHECK(t.sizes() == expected_sizes, "All tensors in the tensor list must have the same size.");
+    TORCH_CHECK(t.dtype() == expected_dtype, "All tensors in the tensor list must have the same dtype.");
+    TORCH_CHECK(t.device() == expected_device, "All tensors in the tensor list must have the same device.");
+  }
+}
+
+// To go via 'fast' path, several conditions must be satisfied 
+// - All tensors must have strided layout
+// - All tensors must be non-overlapping and dense
+// - Resulting tensor must have the same dtype as the input one
+bool check_fast_route(TensorList tensors, Scalar scalar) {
+  TORCH_CHECK(tensors.size() > 0, "Tensor list must have at least one tensor.");
+
+  for (auto t : tensors) {
+    if (t.layout() != at::kStrided) {
+      return false;
+    }
+
+    if (!t.is_non_overlapping_and_dense()) {
+      return false;
+    }
+
+    // complex scalar + integral or boolean tensor will result in complex tensor
+    if (scalar.isComplex() && at::isIntegralType(t.scalar_type(), /*includeBool*/ true)) {
+      return false;
+    }
+
+    // float scalar + integral or boolean tensor will result in float tensor
+    if (scalar.isFloatingPoint() && at::isIntegralType(t.scalar_type(), /*includeBool*/ true)) {
+      return false;
+    }
+
+    // integral scalar + boolean tensor will result in integral tensor 
+    if (scalar.isIntegral(/*includeBool*/ false) && t.dtype() == at::kBool) {
+      return false;
+    }
+  }
+
+  return true;
+}
+}} // at::native
