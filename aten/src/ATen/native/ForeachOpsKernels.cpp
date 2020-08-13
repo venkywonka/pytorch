e57351532d130ea9500104ee7810a3ed5025a4b5
Iurii Zdebskyi <iuriiz@devfair004.maas>
2020-08-12 19:52:05 -0700

    Added add_list APIs
    ghstack-source-id: 3093b2159de552b291e34cca9fec411f650e19cf
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/42533

--- aten/src/ATen/native/ForeachOpsKernels.cpp
+++ aten/src/ATen/native/ForeachOpsKernels.cpp
@@ -11,6 +11,7 @@ std::vector<Tensor> foreach_tensor_add_scalar_kernel_slow(TensorList tensors, Sc
     auto temp = t.add(scalar);
     result.emplace_back(temp);
   }
+
   return result;
 }
 
@@ -19,6 +20,26 @@ void foreach_tensor_add_scalar_kernel_slow_(TensorList tensors, Scalar scalar) {
 
   for (auto& t : tensors) {
     t.add_(scalar);
+  }
+}
+
+std::vector<Tensor> foreach_tensor_add_list_kernel_slow(TensorList tensors1, TensorList tensors2) {
+  verify_list(tensors1, tensors2);
+
+  std::vector<Tensor> result;
+  for (int i = 0; i < tensors1.size(); i++) {
+    auto temp = tensors1[i].add(tensors2[i]);
+    result.emplace_back(temp);
+  }
+
+  return result;
+}
+
+void foreach_tensor_add_list_kernel_slow_(TensorList tensors1, TensorList tensors2) {
+  verify_list(tensors1, tensors2);
+
+  for (int i = 0; i < tensors1.size(); i++) {
+    tensors1[i].add_(tensors2[i]);
   }
 }
 
--- aten/src/ATen/native/ForeachUtils.h
+++ aten/src/ATen/native/ForeachUtils.h
@@ -3,6 +3,7 @@
 
 namespace at { 
 namespace native {
+namespace {
 
 void verify_list(TensorList tensors) {
   TORCH_CHECK(tensors.size() > 0, "Tensor list must have at least one tensor.");
@@ -10,16 +11,16 @@ void verify_list(TensorList tensors) {
   auto expected_device = tensors[0].device();
   auto expected_sizes = tensors[0].sizes();
 
-  for (auto t : tensors) {
+  for (const auto& t : tensors) {
     TORCH_CHECK(t.sizes() == expected_sizes, "All tensors in the tensor list must have the same size.");
     TORCH_CHECK(t.dtype() == expected_dtype, "All tensors in the tensor list must have the same dtype.");
     TORCH_CHECK(t.device() == expected_device, "All tensors in the tensor list must have the same device.");
   }
 }
 
-// To go via 'fast' path, several conditions must be satisfied 
+// In order to go via 'fast' path, sevelar conditions must be satisfied 
 // - All tensors must have strided layout
-// - All tensors must be non-overlapping and dense
+// - All tensors must be non overlapping and dense
 // - Resulting tensor must have the same dtype as the input one
 bool check_fast_route(TensorList tensors, Scalar scalar) {
   TORCH_CHECK(tensors.size() > 0, "Tensor list must have at least one tensor.");
@@ -50,5 +51,48 @@ bool check_fast_route(TensorList tensors, Scalar scalar) {
   }
 
   return true;
+}
+
+void verify_list(TensorList tensors1, TensorList tensors2) {
+  TORCH_CHECK(tensors1.size() > 0, "Tensor list must have at least one tensor.");
+  TORCH_CHECK(tensors2.size() > 0, "Tensor list must have at least one tensor.");
+  TORCH_CHECK(tensors1.size() == tensors2.size(), "Tensor lists must have the same number of tensors.");
+
+  auto expected_dtype = tensors1[0].dtype();
+  auto expected_device = tensors1[0].device();
+  auto expected_sizes = tensors1[0].sizes();
+
+  for (int i = 0; i < tensors1.size(); i++) {
+    TORCH_CHECK(tensors1[i].sizes() == expected_sizes, "All tensors in the tensor list must have the same size.");
+    TORCH_CHECK(tensors1[i].dtype() == expected_dtype, "All tensors in the tensor list must have the same dtype.");
+    TORCH_CHECK(tensors1[i].device() == expected_device, "All tensors in the tensor list must have the same device.");
+    TORCH_CHECK(tensors2[i].sizes() == expected_sizes, "All tensors in the tensor list must have the same size.");
+    TORCH_CHECK(tensors2[i].dtype() == expected_dtype, "All tensors in the tensor list must have the same dtype.");
+    TORCH_CHECK(tensors2[i].device() == expected_device, "All tensors in the tensor list must have the same device.");
+  }
+}
+
+bool check_fast_route(TensorList tensors1, TensorList tensors2) {
+  for (int64_t i = 0; i < tensors1.size(); i++) {
+    TORCH_CHECK(tensors1[i].sizes() == tensors2[i].sizes(), "Corresponding tensors from tensor lists have different size.");
+
+    if (tensors1[i].layout() != at::kStrided || 
+        tensors2[i].layout() != at::kStrided) {
+      return false;
+    }
+
+    if (tensors1[i].strides() != tensors2[i].strides()) {
+      return false;
+    }
+
+    if (!tensors1[i].is_non_overlapping_and_dense() || 
+        !tensors2[i].is_non_overlapping_and_dense()) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 }
 }} // at::native
